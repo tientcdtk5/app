aef5c88fdfd0c32bbc8b0f4660e4de9f522d329e
tientcdtk5 <manhtien150799@gmail.com>
2023-08-04 20:11:52 +0700

    Add new project food

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chào Ly!',
+    introDesc: `a có câu hỏi dành cho emmm`,
+    btnIntro: '??????',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: ' ',
+    btnYes: 'đúng rồi anh <33',
+    btnNo: 'Không',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Tại vì đó là anh',
+    mess: 'Anh biết mà, nhưng a chê !!!',
+    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100011409428182' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
